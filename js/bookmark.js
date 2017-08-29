9b7c04b389b79a2f598f15ce3c4480f94989b048
24x7test <31246123+24x7test@users.noreply.github.com>
2017-08-29 23:13:59 +0700

    cm all

---
+++ js/bookmark.js
@@ -0,0 +1,20 @@
+﻿function CreateBookmarkLink(url, title) {
+    if (window.sidebar && window.sidebar.addPanel) {   // FF ver 23 ����.
+        window.sidebar.addPanel(title, url, '');
+    } else {
+        if (window.external && ('AddFavorite' in window.external)) {    // IE
+            window.external.AddFavorite(url, title);
+        } else {    // Others
+            alert('Ȯ���� Ŭ���Ͻ� �� �ּ�â���� <Ctrl+D>�� �����ø� ���ã�⿡ ��ϵ˴ϴ�.');
+        }
+    }
+}
+
+function viewXsheild() {
+    window.open("/html/xsheild.html","xsheild","height=460,width=400,scrollbars=no");
+}
+
+function viewsafeshop() {
+    window.open("/html/safeshop.html","safe","height=215,width=350,scrollbars=no");
+}
+
